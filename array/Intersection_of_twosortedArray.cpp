a2e33e02ec7154a262a5b149b6216ca00dc93ad8
pushkar kumar <ssidhant511@gmail.com>
2023-03-28 21:41:41 +0530

    commit

---
+++ array/Intersection_of_twosortedArray.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findIntersection(vector<int> &a, vector<int> &b)
+{
+    vector<int> ans;
+    int n = a.size();
+    int m = b.size();
+    vector <int> visited(m, 0);  
+    // int visited[m] = {0}; // this created to maintain a visitation of every array
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i] == b[j] && visited[j] == 0)
+            {
+                ans.push_back(a[i]);
+                visited[j] = 1;
+                break;
+            }
+
+            else if (b[j] > a[i])
+            {
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> a{1, 2, 2, 3, 3, 4, 5, 6};
+    vector<int> b{2, 3, 3, 5, 6, 6, 7};
+
+    vector<int> ans = findIntersection(a, b);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
--- array/Move_zeros_toEnd.cpp
+++ array/Move_zeros_toEnd.cpp
@@ -3,30 +3,78 @@ using namespace std;
  
  void MoveZeros(int arr[], int n){
     vector<int> temp;
+    //step 1 if you  are a non zero number then put it in the temp
     for(int i=0; i<n; i++){
         if(arr[i]!=0){
             temp.push_back(arr[i]);
         }
     }
-
+    //step 2 put all the temp number in the front then
     int nz= temp.size();
     for(int i=0; i<nz; i++){
         arr[i]= temp[i];
     }
+    //step 3  then filling of zeros
     for(int i=nz; i<n; i++){
         arr[i]=0;
     }
+
+//time complexity -> O(2N)
+//space complexity -> O(2N)
  }
+
+
+//*********************Optimal COde*********************************
+
+ void MoveIngZeroOptimalcode(int a[], int n){
+    int k= 0;
+    while(k<n){
+        if(a[k]==0){
+            break;
+        }
+        else{
+            k= k+1;
+        }
+    }
+
+    int i=k;
+    int j = k+1;
+
+    while (i<n && j<n)
+    {
+        if(a[j]!=0){
+            swap(a[i], a[j]);
+            i++;
+        }
+        j++;
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    
+ }
+ 
+
  
 int main(){
     int arr[]= {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
     int n= sizeof(arr)/sizeof(int);
-
+//****************brute force code
     MoveZeros(arr, n);
 
     for(int i=0; i<n; i++){
         cout<<arr[i]<<"  ";
     }
+
+
+//*********************Optimal COde*********************************
+    
+    int a[]= {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n= sizeof(a)/sizeof(int);
+    
+    MoveIngZeroOptimalcode(a, n);
+
  
   return 0;
 }
